5bb6e7a33e9457f461f647c60c95e2b338c7a2b5
alekseik1 <1alekseik1@gmail.com>
2018-10-25 19:17:51 +0300

    Сделал задачу на сложение чисел (НЕ проверял)

---
+++ week1/sum/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Number} a Первое слагаемое
+ * @param {Number} b Второе слагаемое
+ * @returns {Number}
+ */
+module.exports = function (a, b) {
+    var a_n = new Number(a);
+    var b_n = new Number(b);
+    return (a_n + b_n);
+};
