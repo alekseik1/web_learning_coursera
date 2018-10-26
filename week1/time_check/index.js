4b2384e239821bdf7dd7b441b464108d797b7ab2
alekseik1 <1alekseik1@gmail.com>
2018-10-26 13:41:17 +0300

    FIX: исправил все ошибки в `week1/time_check`

--- week1/time_check/index.js
+++ week1/time_check/index.js
@@ -4,7 +4,7 @@
  * @returns {Boolean}
  */
 module.exports = function (hours, minutes) {
-    if(0 > hours || 60 <= minutes)
+    if(0 > hours || 60 <= minutes || 24 <= hours || 0 > minutes)
         return false;
     return true;
 };
