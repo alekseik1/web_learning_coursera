f90735e98ff64b3c559c4363c20d7941480c523c
alekseik1 <1alekseik1@gmail.com>
2018-10-27 11:38:29 +0300

    FIX: добился правильной работы задание `phone book`

--- week2/phone_book/index.js
+++ week2/phone_book/index.js
@@ -2,8 +2,8 @@
 var phoneBook = {};
 
 
-showBook() {
-    var keys = phoneBook.keys(),
+function showBook() {
+    var keys = Object.keys(phoneBook),
         result = [];
     for(var i = 0; i < keys.length; i++) {
         var man = keys[i],
@@ -11,28 +11,29 @@ showBook() {
         if(numbers.length === 0)
             continue;
         else
-            results.push(man + ": " + numbers.join(", "));
+            result.push(man + ": " + numbers.join(", "));
     }
-    return result;
+    return result.sort();
 }
 
-addNumber(person, number) {
+function addNumber(person, number) {
     if(!phoneBook.hasOwnProperty(person))
         phoneBook[person] = [];
-    else
-        phoneBook[person].push(number);
+    phoneBook[person].push(number);
 }
 
-removePhone(number) {
-    var keys = phoneBook.keys(),
+function removePhone(number) {
+    var keys = Object.keys(phoneBook),
         result = false;
     for(var i = 0; i < keys.length; i++) {
         var acc_init = [];
         phoneBook[keys[i]] = phoneBook[keys[i]].reduce((acc, item) => {
             if(item !== number) acc.push(item);
+            else result = true;
             return acc;
         }, acc_init);
     }
+    return result;
 }
 
 /**
@@ -43,8 +44,14 @@ module.exports = function (command) {
     if(command === "SHOW") {
         return showBook();
     } else if(command.startsWith("ADD")) {
-               
+        var args = command.split(' ').slice(1);
+        person = args[0];
+        numbers = args[1].split(',');
+        for (var i=0; i<numbers.length; i++) {
+            addNumber(person, numbers[i]);
+        }
     } else if(command.startsWith("REMOVE_PHONE")) {
-
+        var number = command.split(' ')[1];
+        return removePhone(number);
     }
 };
